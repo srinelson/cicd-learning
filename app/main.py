615ecb29a47a9c711fa975f91f6c544b008fb3fe
srinelson <116962150+srinelson@users.noreply.github.com>
2026-07-13 23:16:12 +0800

    Add proper spacing between functions

--- app/main.py
+++ app/main.py
@@ -2,13 +2,16 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return jsonify({"message": "CI/CD learning app is running"})
 
+
 @app.route("/health")
 def health():
     return jsonify({"status": "healthy"}), 200
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
